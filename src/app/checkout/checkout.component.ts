2fa4685f85ce50897d565df52afc2ce0918c3927
mahrokh sadro <mahrokh.sadro@j-sas.com>
2025-03-14 14:11:06 -0400

    setting stripe in UI

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {MatStepperModule} from '@angular/material/stepper';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { Location } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { AsyncPipe } from '@angular/common';
+
+
+import { OrderSummaryComponent } from '../order-summary/order-summary.component';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [
+    OrderSummaryComponent,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    AsyncPipe,
+  ],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss'
 })
 export class CheckoutComponent {
+  private location=inject(Location);
+  constructor(){
+     console.log('path',this.location.path())
+  }
 
+  private _formBuilder = inject(FormBuilder);
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  thirdFormGroup = this._formBuilder.group({
+    thirdCtrl: ['', Validators.required],
+  });
 }
--- src/app/order-summary/order-summary.component.ts
+++ src/app/order-summary/order-summary.component.ts
@@ -1,16 +1,19 @@
 import { Component, inject } from '@angular/core';
+import { RouterModule  } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-order-summary',
-  imports: [],
+  imports: [RouterModule ],
   templateUrl: './order-summary.component.html',
   styleUrl: './order-summary.component.scss',
   standalone: true,
 
 })
 export class OrderSummaryComponent {
-   
     cartService=inject(CartService);
-  
+    location=inject(Location);
+
 }
